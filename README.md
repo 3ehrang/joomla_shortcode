e4bd793a5a98afb4fee24b56642036120f7ce193
3ehrang <3ehrang@gmail.com>
2018-02-28 12:03:54 +0330

    Add joomla tag short code.

--- README.md
+++ README.md
@@ -6,8 +6,15 @@ Insert Joomla article with title, image, intro text , ... inside and anywhere in
 - inside your joomla administrator panel go to extentions -> manage -> install and choose "update package file" tab and upload it.
 - go to extentions -> plugins and find shortcode plugin enable and customize it.
 
-## How To Use
+## Supported Shortcode
+
+### Show single article inside another article
+
 - Inside your article add/edit back-end page any where you want to show another article use this code:
-  {article id} like {article 23}
-- Goto article front-end page and see the result
+  ``{article id} like {article 23}``
+  
+## Show a list of articles inside specific tag
+
+- Inside your article add/edit back-end page any where you want to show articles use this code:
+  ``{tag id} like {tag 18}``
   
--- shortcode.php
+++ shortcode.php
@@ -38,7 +38,6 @@ class PlgContentShortcode extends JPlugin
 	 */
 	public function onContentPrepare($context, &$article, &$params, $page = 0)
 	{
-	    
 	    // Don't run this plugin when the content is being indexed
 	    if ($context == 'com_finder.indexer')
 	    {
@@ -50,9 +49,17 @@ class PlgContentShortcode extends JPlugin
 		{
 			return true;
 		}
+		// Simple performance check to determine whether bot should process further
+		if (strpos($article->text, 'tag') === false)
+		{
+			return true;
+		}
 
 		// Expression to search for (article)
-		$regex		= '/{article\s(.*?)}/i';
+		$regexArticle	= '/{article\s(.*?)}/i';
+		// Expression to search for (tag)
+		$regexTag		= '/{tag\s(.*?)}/i';
+		
 		/**
 		 * @todo get params from plugin like position and add it to article params
 		 */
@@ -60,17 +67,35 @@ class PlgContentShortcode extends JPlugin
 
 		// Find all instances of plugin and put in $matches for article
 		// $matches[0] is full pattern match, $matches[1] is the id or alias
-		preg_match_all($regex, $article->text, $matches, PREG_SET_ORDER);
+		preg_match_all($regexArticle, $article->text, $matchesArticle, PREG_SET_ORDER);
+		preg_match_all($regexTag, $article->text, $matchesTag, PREG_SET_ORDER);
 
 		// No matches, skip this
-		if ($matches)
+		if ($matchesArticle)
 		{
-			foreach ($matches as $match)
+			foreach ($matchesArticle as $matchArticle)
 			{
-				$inArticle	= $this->_getArticle($match[1]);
-				$output 	= $this->_outputArticle($inArticle);
+				$inArticle	= $this->_getArticle($matchArticle[1]);
+				$link 		= $this->_outputArticleLink($inArticle);
+				$output 	= $this->_outputArticle($link);
 				// We should replace only first occurrence in order to allow other shortcode with the same name to regenerate their content:
-				$article->text = preg_replace("|$match[0]|", addcslashes($output, '\\$'), $article->text, 1);
+				$article->text = preg_replace("|$matchArticle[0]|", addcslashes($output, '\\$'), $article->text, 1);
+			}
+		}
+		
+		// No matches, skip this
+		if ($matchesTag)
+		{
+			foreach ($matchesTag as $matcheTag)
+			{
+				$tagArticles	= $this->_getArticlesByTag($matcheTag[1]);
+				$output = '<ul class="shortc-article-list">';
+				foreach ($tagArticles as $tagArticle) {
+					$output	.= '<li>' . $this->_outputArticleLink($tagArticle) . '</li>';
+				}
+				$output .= '</ul>';
+				// We should replace only first occurrence in order to allow other shortcode with the same name to regenerate their content:
+				$article->text = preg_replace("|$matcheTag[0]|", addcslashes($output, '\\$'), $article->text, 1);
 			}
 		}
 	}
@@ -88,17 +113,49 @@ class PlgContentShortcode extends JPlugin
 	}
 	
 	/**
-	 * Get article and output title, image , ...
+	 * Get tag by id or alias
+	 *
+	 * @param integer $articleId
+	 * @return object
+	 */
+	protected function _getArticlesByTag($tagId)
+	{
+		// Get an instance of the generic articles model
+		$model = JModelLegacy::getInstance('Articles', 'ContentModel');
+		// call getState to enable filtering
+		$model->getState();
+		// Filer by tag
+		$model->setState('filter.tag', $tagId);
+		
+		$articles = $model->getItems();
+		return $articles;
+	}
+	
+	/**
+	 * Format article link 
 	 * 
+	 * @param string $articleLink
+	 * @return string
+	 */
+	protected function _outputArticle($articleLink)
+	{
+		// format article
+		$output = '<div class="shortc-article">' . $articleLink . '</div>';
+		return $output;
+	}
+	
+	/**
+	 * Get article and output article link with image, intro, ...
+	 *
 	 * @param object $article
 	 * @return string
 	 */
-	protected function _outputArticle($article)
+	protected function _outputArticleLink($article)
 	{
 		// get article sef url
 		$url = JRoute::_(ContentHelperRoute::getArticleRoute($article->id,  $article->catid));
 		
-		$output = '<a class="inline-article" href="' . $url . '">';
+		$output = '<a href="' . $url . '">';
 		
 		// add intro image
 		if ($this->params->get('show_image'))
